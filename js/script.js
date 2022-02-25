7d6ac2935e74ed48959b898152988e0895535a8c
Nicolas Quinones <quinonesnn@gmail.com>
2022-02-24 20:55:53 -0500

    checks if table cells are empty horizontally and vertically, working on winstate

--- js/script.js
+++ js/script.js
@@ -7,33 +7,111 @@ window.onload = function () {
 }
 
 function switch_elems(i, j) {
+    console.clear()
     console.log("-------------------")
     const table = document.querySelector('table');
-    const val1 = table.rows[i].cells[j].innerHTML;
-    console.log("val1 " + val1)
+    let currentData = [...table.rows].map(t => [...t.children].map(u => u.innerText))
+    const winState = [["1","2","3"],["8","","4"],["7","6","5"]]
+    const selectedBlock = table.rows[i].cells[j].innerHTML;
+    console.log("selectedBlock " + selectedBlock)
 
     console.log("i " + i)
-    console.log("j " + i)
+    console.log("j " + j)
 
-    console.log("we take j:" + j + " and add 1")
-    let k = j + 1;
-    console.log("k before " + k)
 
-    let numRows = table.rows.length; // not used, but this gets num rows
-    let sub = table.rows[i].cells.length - 1;
-    console.log("if k is less than table.rows[" + i + "].cells.length - 1 =" + sub) 
-    if (k > table.rows[i].cells.length - 1) {
-        k = 0;
-        console.log("k was turned to 0")
+    switch(checkDirection(currentData,i,j)){
+        case 1:
+            console.log("case1")
+            emptyBlock = table.rows[i].cells[j + 1].innerHTML
+            table.rows[i].cells[j].innerHTML = emptyBlock.toString();
+            table.rows[i].cells[j + 1].innerHTML = selectedBlock.toString();
+            checkWin(currentData,winState)
+            break;
+        case 2:
+            console.log("case2")
+            emptyBlock = table.rows[i].cells[j - 1].innerHTML
+            table.rows[i].cells[j].innerHTML = emptyBlock.toString();
+            table.rows[i].cells[j - 1].innerHTML = selectedBlock.toString();
+            checkWin(currentData,winState)
+            break;
+        case 3:
+            console.log("case3")
+            emptyBlock = table.rows[i + 1].cells[j].innerHTML
+            table.rows[i].cells[j].innerHTML = emptyBlock.toString();
+            table.rows[i + 1].cells[j].innerHTML = selectedBlock.toString();
+            checkWin(currentData,winState)
+            break;
+        case 4:
+            console.log("case4")
+            emptyBlock = table.rows[i - 1].cells[j].innerHTML
+            table.rows[i].cells[j].innerHTML = emptyBlock.toString();
+            table.rows[i - 1].cells[j].innerHTML = selectedBlock.toString();
+            checkWin(currentData,winState)
+            break;
     }
-    console.log("k after stayed the same")
+}
 
-    const val2 = table.rows[i].cells[k].innerHTML;
-    console.log("val2 " + val2)
+function isEmpty(val){
+    if(val === ''){
+        return true
+    }
+    return false
+}
 
+function checkDirection(currentData, i, j){
+    let rowLen = currentData.length - 1;
+    let colLen = currentData[i].length - 1;
+    console.log("checking... ")
+    if(j < rowLen){
+        //right
+        console.log("checking right...")
+        if(isEmpty(currentData[i][j + 1])){
+            console.log("right was empty")
+            return 1;
+        }
+        else{
+            console.log('not right')
+        }
+    }
+    if(j != 0){
+        //left
+        console.log("checking left...")
+        if(isEmpty(currentData[i][j - 1])){
+            console.log("left was empty")
+            return 2;
+        }
+        else{
+            console.log('not left')
+        }
+    }
+    if(i < colLen){
+        //down
+        console.log("checking down...")
+        if(isEmpty(currentData[i + 1][j])){
+            console.log("down was empty")
+            return 3;
+        }
+        else{
+            console.log('not down')
+        }
+    }
+    if(i != 0){
+        //up
+        console.log("checking up...")
+        if(isEmpty(currentData[i - 1][j])){
+            console.log("up was empty")
+            return 4;
+        }
+        else{
+            console.log('not up')
+        }
+    }
+}
 
-    table.rows[i].cells[j].innerHTML = val2.toString();
-    table.rows[i].cells[k].innerHTML = val1.toString();
-    console.log("-------------------")
-
+function checkWin(currentData, winState){
+    console.log(JSON.stringify(currentData))
+    console.log(JSON.stringify(winState))
+    if(JSON.stringify(currentData) === JSON.stringify(winState)){
+        console.log(" YOU WON ")
+    }
 }
